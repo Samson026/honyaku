fe5b9e67d33443e341faaee3febfc88e8ae6908d
Samuel Dietz <samdietz2001@gmail.com>
2026-05-01 16:51:12 +1000

    feat-echo-server

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,44 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN
+
+async function reply(replyToken: String, text: String) {
+  await fetch('https://api.line.me/v2/bot/message/reply', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      replyToken,
+      messages: [
+        {
+          type: 'text',
+          text
+        }
+      ]
+    })
+  })
+}
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post('/webhook', async (c) => {
+  const data = await c.req.json();
+
+  console.log(CHANNEL_ACCESS_TOKEN)
+
+  for (const event of data.events) {
+    if (event.type === 'message') {
+      console.log(event.message.text)
+      await reply(event.replyToken, event.message.text)
+    }
+  }
+  
+  return c.json({status: "ok"})
 })
 
 serve({
