b2885e2c94bfc32da1c50c02c74957d44821b854
Samuel Dietz <samdietz2001@gmail.com>
2026-07-15 13:16:36 +1000

    fix-bug-for-multi-user

--- src/translator.ts
+++ src/translator.ts
@@ -5,6 +5,11 @@ import Anthropic from "@anthropic-ai/sdk";
 const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN
 const CLAUDE_API_KEY = process.env.CLAUDE_API_KEY
 
+type RespItem = {
+  type: "text",
+  text: string
+} 
+
 const reply = new Hono()
 const client = new Anthropic({
   apiKey: CLAUDE_API_KEY
@@ -31,7 +36,11 @@ async function Translate(language: string, text: string) {
   throw new Error("no text block in Anthropic response")
 }
 
-async function ReplyToMessage(replyToken: string, text: string) {
+async function ReplyToMessage(replyToken: string, resp: RespItem[]) {
+  if (resp.length === 0) {
+    return
+  }
+
   await fetch('https://api.line.me/v2/bot/message/reply', {
     method: 'POST',
     headers: {
@@ -40,12 +49,7 @@ async function ReplyToMessage(replyToken: string, text: string) {
     },
     body: JSON.stringify({
       replyToken,
-      messages: [
-        {
-          type: 'text',
-          text
-        }
-      ]
+      messages: resp
     })
   })
 }
@@ -54,6 +58,7 @@ async function group_translate(event: any) {
   const groupID = event.source.groupId
   const groupMembers = await GetGroupMembers(groupID)
   var senderName = 'None';
+  var messages: RespItem[] = []
 
   for (const user of groupMembers) {
     if (user.id === event.source.userId) {
@@ -69,13 +74,13 @@ async function group_translate(event: any) {
     }
     console.log(senderName)
     const translation = await Translate(user.lang, event.message.text)
-    if (translation === "null") {
-      // message is already in target language
-      return
+    if (translation !== "null") {
+      // message is not in target language
+      const reply = `${senderName}:\n${translation}`
+      messages.push({type: "text", text: reply})
     }
-    const reply = `${senderName}:\n${translation}`
-    await ReplyToMessage(event.replyToken, reply)
   }
+  await ReplyToMessage(event.replyToken, messages)
 }
 
 async function set_language_reply(event: any) {
@@ -91,7 +96,7 @@ async function set_language_reply(event: any) {
     await AddUserToGroup(userID, groupID, language, user.displayName)
 
     const reply = `set user: ${user.displayName}'s translation language to ${language}`
-    await ReplyToMessage(event.replyToken, reply)
+    await ReplyToMessage(event.replyToken, [{type: "text", text: reply}])
 }
 
 reply.post('/', async (c) => {
